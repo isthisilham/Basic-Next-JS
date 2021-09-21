0a7529b171edf02e9b81ed24fef5b2a46a903fd5
Muhammad Ilham <muhammadilham6656@gmail.com>
2021-09-21 12:12:13 +0700

    Routing Page - NextJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
+    <>
+    <header>
+      <ul>
+        <li>
+          <Link href="/"><a>Home</a></Link>
+        </li>
+        <li>
+          <Link href="/about"><a>about</a></Link>
+        </li>
+        <li>
+          <Link href="/users"><a>users</a></Link>
+        </li>
+      </ul>
+    </header>
     <h1>Welcome Muhammad Ilham</h1>
+    </>
   )
 }
 
